d0ce93c169b1a8c00a65e1573718e9920426b9f6
Saeid <saeed221373@yahoo.com>
2025-08-19 16:47:32 +0330

    change new function

--- src/AIBasedBadge.sol
+++ src/AIBasedBadge.sol
@@ -38,5 +38,13 @@ contract AIBasedBadge is Ownable {
         return totalMints;
     }
 
+    function getUserCollectionsCount(address user) external view returns (uint256) {
+        uint256 totalCollections = 0;
+        for (uint256 i = 0; i < factories.length; i++) {
+            totalCollections += AIBasedNFTFactory(factories[i]).getUserCollectionsCount(user);
+        }
+        return totalCollections;
+    }
+
     
 }
