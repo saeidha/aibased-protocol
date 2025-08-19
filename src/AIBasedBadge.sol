c32be7432b5f31f1cf05c28c718985225b56f26d
Saeid <saeed221373@yahoo.com>
2025-08-19 16:48:08 +0330

    change new function

--- src/AIBasedBadge.sol
+++ src/AIBasedBadge.sol
@@ -4,9 +4,7 @@ pragma solidity 0.8.24;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface AIBasedNFTFactory {
-    function getUserMintCount(address user) external view returns (uint256);
-    function getUserCollectionsCount(address user) external view returns (uint256);
-    function getUserPayGenerateFeeCount(address user) external view returns (uint256);
+    
 }
 
 contract AIBasedBadge is Ownable {
