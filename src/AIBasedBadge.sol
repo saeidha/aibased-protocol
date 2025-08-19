f8dca13d3361087ce9ec6e3532c9b3c50be90b1c
Saeid <saeed221373@yahoo.com>
2025-08-19 16:48:13 +0330

    change new function

--- src/AIBasedBadge.sol
+++ src/AIBasedBadge.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.24;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface AIBasedNFTFactory {
+    function getUserMintCount(address user) external view returns (uint256);
     
 }
 
