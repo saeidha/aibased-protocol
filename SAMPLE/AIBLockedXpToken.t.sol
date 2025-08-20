0bb01be7d959671d62ba32258f366940cf503d97
Saeid <saeed221373@yahoo.com>
2025-08-20 17:23:09 +0330

    add AIBLockedXpToken.t.sol

--- SAMPLE/AIBLockedXpToken.t.sol
+++ SAMPLE/AIBLockedXpToken.t.sol
@@ -3,3 +3,9 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {AIBLockedXpToken} from "../src/AIBLockedXpToken.sol";
+
+contract AIBLockedXpTokenTest is Test {
+
+
+
+}
