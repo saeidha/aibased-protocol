a848470fd0cfc4b9bc93a3882d75a304bec0a5aa
Saeid <saeed221373@yahoo.com>
2025-08-20 17:16:54 +0330

    add contract AIBLockedXpToken

---
+++ SAMPLE/AIBLockedXpToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract AIBLockedXpToken is ERC20, Ownable {
+
+}
