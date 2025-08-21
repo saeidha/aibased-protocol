5967dbc0a9826d544142e382f5dd27e588d14242
saeidha <saeed221373@yahoo.com>
2025-08-21 19:30:17 +0330

    change on src/LockToken.sol Solidity contract Adjustment

--- SAMPLE/LockToken.sol
+++ SAMPLE/LockToken.sol
@@ -110,3 +110,6 @@ emit TokensLocked(msg.sender, lockId, _amount, unlockTime);
      * @return The lock details: owner, amount, unlockTime, and active status.
      */
     function getLockDetails(uint256 _lockId) external view returns (address, uint256, uint256, bool) {
+        Lock memory userLock = locks[_lockId];
+        return (userLock.owner, userLock.amount, userLock.unlockTime, userLock.active);
+    }
