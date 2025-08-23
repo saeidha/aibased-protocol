81ef4328b2d14620859b1e834f3a4c6b86707ba7
Saeid <saeed221373@yahoo.com>
2025-08-23 15:35:39 +0330

    change on SAMPLE/LockToken.t.sol Solidity contract Refactor

--- SAMPLE/LockToken.t.sol
+++ SAMPLE/LockToken.t.sol
@@ -7,3 +7,9 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract LockToken is Ownable, Pausable {
     using SafeERC20 for IERC20;
+
+    // --- Events ---
+    event TokensLocked(address indexed user, uint256 lockId, uint256 amount, uint256 unlockTime);
+    event TokensWithdrawn(address indexed user, uint256 lockId, uint256 amount);
+    event LockExtended(uint256 indexed lockId, uint256 newUnlockTime);
+    event EmergencyWithdrawal(address indexed token, address indexed to, uint256 amount);
