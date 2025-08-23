a6c923fcfc957a8f53de714b62ae1346ff848ac1
Saeid <saeed221373@yahoo.com>
2025-08-23 14:50:30 +0330

    add contract

--- SAMPLE/MockERC20.sol
+++ SAMPLE/MockERC20.sol
@@ -4,3 +4,6 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {LockToken} from "../src/LockToken.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// A simple Mock ERC20 for testing purposes
+contract MockERC20 is IERC20 {
