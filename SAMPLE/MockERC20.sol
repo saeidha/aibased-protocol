0300b03e72f36c1289b317f1fdbce11f727ee133
Saeid <saeed221373@yahoo.com>
2025-08-23 14:51:17 +0330

    add contract approve

--- SAMPLE/MockERC20.sol
+++ SAMPLE/MockERC20.sol
@@ -19,3 +19,5 @@ function balanceOf(address account) external view returns (uint256) {
     function allowance(address owner, address spender) external view returns (uint256) {
         return allowances[owner][spender];
     }
+
+        function approve(address spender, uint256 amount) external returns (bool) {
