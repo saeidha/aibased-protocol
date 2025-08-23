7571d03b6b24fcdf67100f18f72751a01e7c3ba0
Saeid <saeed221373@yahoo.com>
2025-08-23 16:14:22 +0330

    change on SAMPLE/LockToken.t.sol Solidity contract Component

--- SAMPLE/LockToken.t.sol
+++ SAMPLE/LockToken.t.sol
@@ -46,3 +46,5 @@ contract LockToken is Ownable, Pausable {
         function lock(uint256 _amount, uint256 _duration) external whenNotPaused {
                     require(_amount > 0, "Amount must be greater than zero");
         require(_duration > 0, "Duration must be greater than zero");
+uint256 lockId = locks.length;
+        uint256 unlockTime = block.timestamp + _duration;
