4f49aa5dc59a7468791694e8a8ca68e1dbd47eda
saeidha <saeed221373@yahoo.com>
2025-08-24 15:41:32 +0330

    change on sample2/Loanly.sol Solidity contract Module

--- sample2/Loanly.sol
+++ sample2/Loanly.sol
@@ -35,6 +35,6 @@ contract Loanly {
      * @param _duration The duration of the loan in seconds.
      */
         function requestLoan(uint256 _amount, uint256 _interest, uint256 _duration) public {
-                    require(_amount > 0, "Loan amount must be greater than zero");
+            require(_amount > 0, "Loan amount must be greater than zero");
             require(_interest > 0, "Interest rate must be greater than zero");
             require(_duration > 0, "Loan duration must be greater than zero");
