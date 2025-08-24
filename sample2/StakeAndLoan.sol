f2ddf65dcb82346144ba632b1cda0bb91364666c
saeidha <saeed221373@yahoo.com>
2025-08-25 02:04:39 +0330

    change on sample2/StakeAndLoan.sol Solidity contract Feature

---
+++ sample2/StakeAndLoan.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title StakeAndLoan
+ * @dev A contract that allows users to stake collateral tokens and borrow loan tokens.
+ */
+contract StakeAndLoan is Ownable {
+    // --- State Variables ---
+
+    IERC20 public immutable collateralToken;
+    IERC20 public immutable loanToken;
