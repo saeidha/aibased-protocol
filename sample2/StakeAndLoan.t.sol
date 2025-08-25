daf4d90dff404b7e3dd5f0a3cf4632437a6d7e4e
saeidha <saeed221373@yahoo.com>
2025-08-26 00:08:14 +0330

    change on sample2/StakeAndLoan.t.sol Solidity contract Cleanup

--- sample2/StakeAndLoan.t.sol
+++ sample2/StakeAndLoan.t.sol
@@ -4,3 +4,6 @@ pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
 import "../src/StakeAndLoan.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor
