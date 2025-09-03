54ffd3579311a3604f19c3de5fed89e8608cae0d
saeidha <saeed221373@yahoo.com>
2025-09-03 19:08:04 +0330

    change on sample2/ENSResolver.sol Solidity contract Component

--- sample2/ENSResolver.sol
+++ sample2/ENSResolver.sol
@@ -16,3 +16,7 @@ abstract contract Resolver {
  * @dev A flexible resolver contract for the ENS-like registry.
  * It stores various types of records for a given node.
  */
+contract PublicResolver is ERC165 {
+    
+    bytes4 private constant ADDR_INTERFACE_ID = 0x3b3b57de;
+    bytes4 private constant TEXT_INTERFACE_ID = 0x59d1d43c;
