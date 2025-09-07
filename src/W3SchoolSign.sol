7f51e74156bdfe1e54e1e7e7bc3c3ad6b628ffce
saeidha <saeed221373@yahoo.com>
2025-09-08 01:40:08 +0330

    Refactor change on src/W3SchoolSign.sol Solidity contract

--- src/W3SchoolSign.sol
+++ src/W3SchoolSign.sol
@@ -255,3 +255,4 @@ contract W3SchoolSign is Ownable {
     function getCourseFee(uint _courseId) external view courseExists(_courseId) returns (uint) {
         return courses[_courseId].enrollmentFee;
     }
+
