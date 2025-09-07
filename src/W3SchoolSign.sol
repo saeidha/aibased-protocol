0a9bf5de7798974d45da4ac4eb479c9193b69420
saeidha <saeed221373@yahoo.com>
2025-09-08 01:38:24 +0330

    Logic change on src/W3SchoolSign.sol Solidity contract

--- src/W3SchoolSign.sol
+++ src/W3SchoolSign.sol
@@ -165,3 +165,5 @@ contract W3SchoolSign is Ownable {
     function updateCourseFee(uint _courseId, uint _newFee) external onlyAdmin courseExists(_courseId) {
         Course storage course = courses[_courseId];
         course.enrollmentFee = _newFee;
+        emit CourseUpdated(_courseId, course.name, course.description, course.enrollmentFee);
+    }
