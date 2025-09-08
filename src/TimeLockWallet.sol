a136f1f10ad67890f509145672d8a22c0858c79d
saeidha <saeed221373@yahoo.com>
2025-09-08 22:18:30 +0330

    Interface change on src/TimeLockWallet.sol Solidity contract

--- src/TimeLockWallet.sol
+++ src/TimeLockWallet.sol
@@ -330,3 +330,5 @@ contract TimeLockWallet is Ownable, Pausable {
     /**
      * @notice Returns the total sum of all funds currently allocated to active beneficiaries.
      * @return The total amount of locked funds.
+     */
+    function getTotalLockedFunds() public view returns (uint256) {
