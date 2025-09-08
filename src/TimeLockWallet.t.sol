a58d03b1bd42c8531d85984e467ede0c579945b9
saeidha <saeed221373@yahoo.com>
2025-09-08 22:05:31 +0330

    Upgrade change on src/TimeLockWallet.t.sol Solidity contract

--- src/TimeLockWallet.t.sol
+++ src/TimeLockWallet.t.sol
@@ -3,3 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {W3SchoolSign} from "../src/W3SchoolSign.sol";
+
+contract W3SchoolSignTest is Test {
+    W3SchoolSign public w3s;
+
