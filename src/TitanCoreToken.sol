afecba4afcf60ae8af8148f59fa1b62156282d87
saeidha <saeed221373@yahoo.com>
2025-09-11 18:10:13 +0330

    Implement insurance fund for protocol on src/TitanCoreToken.sol

--- src/TitanCoreToken.sol
+++ src/TitanCoreToken.sol
@@ -11,3 +11,6 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+/** 
