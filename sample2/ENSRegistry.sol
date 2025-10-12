1ba5543dcb89b891991c1775242e03e89d49a031
Saeid <5596754+saeidha@users.noreply.github.com>
2025-10-12 15:32:25 +0330

    add abstraction

--- sample2/ENSRegistry.sol
+++ sample2/ENSRegistry.sol
@@ -4,3 +4,11 @@ pragma solidity >=0.8.0 <0.9.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/Pausable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/introspection/IERC165.sol";
+
+abstract contract ENS {
+    function setSubnodeOwner(bytes32 node, bytes32 label, address owner) virtual external;
+    function setResolver(bytes32 node, address resolver) virtual external;
+    function setOwner(bytes32 node, address owner) virtual external;
+    function owner(bytes32 node) virtual external view returns (address);
+    function resolver(bytes32 node) virtual external view returns (address);
+}
