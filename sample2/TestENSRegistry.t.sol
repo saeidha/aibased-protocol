4b3de74e09ddf83dff23e0d71adaac4380b943d4
Saeid <5596754+saeidha@users.noreply.github.com>
2025-10-16 16:57:54 +0330

    add test setOwner

--- sample2/TestENSRegistry.t.sol
+++ sample2/TestENSRegistry.t.sol
@@ -30,3 +30,8 @@ contract TestENSRegistry is Test {
    function test_initialOwner() public {
         assertEq(registry.owner(testNode), user1);
     }
+    function test_setOwner() public {
+        vm.prank(user1);
+        registry.setOwner(testNode, user2);
+        assertEq(registry.owner(testNode), user2);
+    }
