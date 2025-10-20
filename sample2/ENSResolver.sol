1d3c6f2ae6ac746ebc974164554fb778d9dcdca2
Saeid <5596754+saeidha@users.noreply.github.com>
2025-10-20 13:33:48 +0330

    Add abstract Resolver contract to ENSResolver.sol

--- sample2/ENSResolver.sol
+++ sample2/ENSResolver.sol
@@ -4,3 +4,11 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/introspection/ERC165.sol";
 import "https/github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
+
+
+
+abstract contract Resolver {
+    function supportsInterface(bytes4 interfaceID) virtual external pure returns (bool);
+    function addr(bytes32 node) virtual external view returns (address);
+    function setAddr(bytes32 node, address addr) virtual external;
+}
