8c6afb2284a79ca7ef41e18c450765a9491a08bd
Saeid <5596754+saeidha@users.noreply.github.com>
2025-10-22 14:10:01 +0330

    Add SPDX license and import statements

--- sample2/EnglishAuction.sol
+++ sample2/EnglishAuction.sol
@@ -1,1 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
