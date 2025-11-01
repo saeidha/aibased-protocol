36c05c5e5e853fc5267a6cbe19a56c3471eb4ce5
Saeid <5596754+saeidha@users.noreply.github.com>
2025-11-01 12:19:01 +0330

    Update ENSResolver.sol

--- sample2/ENSResolver.sol
+++ sample2/ENSResolver.sol
@@ -1,1 +1,8 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/introspection/ERC165.sol";
+import "https/github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
+
+
