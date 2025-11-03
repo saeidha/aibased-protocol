75f5d1f7feb154cd821013985787d0c3472f468b
Saeid <5596754+saeidha@users.noreply.github.com>
2025-11-03 21:14:33 +0330

    Update StakeAndLoan.sol

--- sample2/StakeAndLoan.sol
+++ sample2/StakeAndLoan.sol
@@ -130,3 +130,27 @@ contract StakeAndLoan is Ownable {
         delete userLoan[msg.sender];
         emit Repaid(msg.sender, totalOwed);
     }
+
+    /**
+     * @dev Liquidates an undercollateralized position.
+     * @param _borrower The address of the borrower to liquidate.
+     */
+    function liquidate(address _borrower) public {
+        uint256 collateralValue = getCollateralValue(stakedBalance[_borrower]);
+        uint256 loanValue = getLoanValue(_borrower);
+        require(loanValue > 0, "No loan to liquidate");
+        require(
+            collateralValue < loanValue,
+            "Position is not undercollateralized"
+        );
+
+        uint256 collateralToLiquidate = stakedBalance[_borrower];
+        stakedBalance[_borrower] = 0;
+        delete userLoan[_borrower];
+        // In a real scenario, this collateral might be auctioned. Here, it's sent to the liquidator.
+        require(
+            collateralToken.transfer(msg.sender, collateralToLiquidate),
+            "Liquidation transfer failed"
+        );
+        emit Liquidated(_borrower, collateralToLiquidate);
+    }
