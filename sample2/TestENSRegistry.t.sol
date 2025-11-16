a9751756eca17d95baa851107f7114905f076266
Saeid <5596754+saeidha@users.noreply.github.com>
2025-11-16 15:44:12 +0330

    Update TestENSRegistry.t.sol

--- sample2/TestENSRegistry.t.sol
+++ sample2/TestENSRegistry.t.sol
@@ -27,10 +27,19 @@ contract TestENSRegistry is Test {
 }
 
    function test_initialOwner() public {
+
         assertEq(registry.owner(testNode), user1);
     }
     function test_setOwner() public {
+
         vm.prank(user1);
         registry.setOwner(testNode, user2);
         assertEq(registry.owner(testNode), user2);
     }
+    function test_setOwner() public {
+
+        vm.prank(user1);
+        registry.setOwner(testNode, user2);
+        assertEq(registry.owner(testNode), user2);
+    }
+    
