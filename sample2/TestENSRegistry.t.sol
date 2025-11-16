060ba6cd5e2e115532cd68bc7529369503c66ac4
Saeid <5596754+saeidha@users.noreply.github.com>
2025-11-16 13:15:19 +0330

    Update TestENSRegistry.t.sol

--- sample2/TestENSRegistry.t.sol
+++ sample2/TestENSRegistry.t.sol
@@ -10,3 +10,6 @@ contract TestENSRegistry is Test {
     address owner = address(0x1);
     address user1 = address(0x2);
     address user2 = address(0x3);
+    bytes32 testNode = keccak256("test");
+    bytes32 testLabel = keccak256("label");
+    bytes32 testSubNode = keccak256(abi.encodePacked(testNode, testLabel));
