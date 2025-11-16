952f5d28fea91f88bed5ea05226583f880af5a92
Saeid <5596754+saeidha@users.noreply.github.com>
2025-11-16 13:10:26 +0330

    Update TestENSRegistry.t.sol

--- sample2/TestENSRegistry.t.sol
+++ sample2/TestENSRegistry.t.sol
@@ -4,3 +4,6 @@ pragma solidity >=0.8.0 <0.9.0;
 import "forge-std/Test.sol";
 import "../contracts/ENSRegistry.sol";
 import "../contracts/PublicResolver.sol";
+contract TestENSRegistry is Test {
+    ENSRegistry registry;
+    PublicResolver resolver;
