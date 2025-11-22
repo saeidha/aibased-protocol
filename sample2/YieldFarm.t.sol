e5fefd85dfb39c3c2a05d9ddd1556433aad72cfe
saeidha <saeed221373@yahoo.com>
2025-11-22 11:18:10 +0330

    feat: Add MockERC20 contract for testing purposes in YieldFarm tests

--- sample2/YieldFarm.t.sol
+++ sample2/YieldFarm.t.sol
@@ -5,3 +5,9 @@ import "forge-std/Test.sol";
 import "../src/YieldFarm.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
+// Mock ERC20 token for testing
+contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    function mint(address to, uint256 amount) public { _mint(to, amount); }
+}
