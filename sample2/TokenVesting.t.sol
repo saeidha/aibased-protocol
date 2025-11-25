e3851fcdd8ec37bcc5d8658b78dcc5a7685f01ff
saeidha <saeed221373@yahoo.com>
2025-11-25 11:24:24 +0330

    feat: Introduce MockToken and TestTokenVesting contracts for enhanced testing of TokenVesting functionality

--- sample2/TokenVesting.t.sol
+++ sample2/TokenVesting.t.sol
@@ -5,3 +5,12 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Test} from "forge-std/Test.sol";
 import {TokenVesting} from "../contracts/TokenVesting.sol";
+
+// Mock ERC20 token for testing purposes
+contract MockToken is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+}
+
+contract TestTokenVesting is Test {
