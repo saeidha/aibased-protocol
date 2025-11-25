f430b1c8f75ac08aa8a2cf1704a2961e7ac58858
saeidha <saeed221373@yahoo.com>
2025-11-25 11:28:11 +0330

    feat: Add initial contract deployment test for TokenVesting to verify token and owner addresses

--- sample2/TokenVesting.t.sol
+++ sample2/TokenVesting.t.sol
@@ -49,3 +49,7 @@ contract TestTokenVesting is Test {
 
         startTime = uint64(block.timestamp + 1 days); // Vesting starts tomorrow
     }
+    function test_01_ContractDeployment() public {
+        assertEq(address(tokenVesting.token()), address(mockToken));
+        assertEq(tokenVesting.owner(), owner);
+    }
