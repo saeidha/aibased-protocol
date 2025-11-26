c7ac38f1f941d8d40b0d19c2aef22d4c7b803fc0
saeidha <saeed221373@yahoo.com>
2025-11-26 14:57:38 +0330

    feat: Extend TestENSRegistry with additional state variables for testing ENSRegistry and PublicResolver

--- sample2/TestENSRegistry.t.sol
+++ sample2/TestENSRegistry.t.sol
@@ -5,3 +5,11 @@ import "forge-std/Test.sol";
 import "../contracts/ENSRegistry.sol";
 import "../contracts/PublicResolver.sol";
 contract TestENSRegistry is Test {
+        ENSRegistry registry;
+    PublicResolver resolver;
+    address owner = address(0x1);
+    address user1 = address(0x2);
+    address user2 = address(0x3);
+    bytes32 testNode = keccak256("test");
+    bytes32 testLabel = keccak256("label");
+    bytes32 testSubNode = keccak256(abi.encodePacked(testNode, testLabel));
