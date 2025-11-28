972187b2058e8067739f0d0e4b6a7faeafbe519a
Saeid <5596754+saeidha@users.noreply.github.com>
2025-11-28 14:47:05 +0330

    Implement loan details retrieval test
    Add test for retrieving loan details including principal, rate, and term.

--- sample2/StakeAndLoan.t.sol
+++ sample2/StakeAndLoan.t.sol
@@ -152,3 +152,19 @@ contract StakeAndLoanTest is Test {
         stakeAndLoan.borrow(maxBorrowable + 1);
         vm.stopPrank();
     }
+
+
+    /**
+     * @dev Tests the ability to get loan details.
+     */
+    function testGetLoanDetails() public {
+        vm.startPrank(user);
+        collateralToken.approve(address(stakeAndLoan), 1 ether);
+        stakeAndLoan.stake(1 ether);
+        stakeAndLoan.borrow(100 ether);
+        (uint256 p, uint256 r, uint256 t) = stakeAndLoan.getLoanDetails(user);
+        assertEq(p, 100 ether);
+        assertEq(r, 500);
+        assertTrue(t > 0);
+        vm.stopPrank();
+    }
