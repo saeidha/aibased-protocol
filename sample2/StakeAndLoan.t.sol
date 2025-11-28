413987ed659a1301094598dc2fd55fcd1c0ca395
Saeid <5596754+saeidha@users.noreply.github.com>
2025-11-28 14:45:15 +0330

    Create MockERC20 contract for testing purposes
    Added a mock ERC20 token contract for testing.

--- sample2/StakeAndLoan.t.sol
+++ sample2/StakeAndLoan.t.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "forge-std/Test.sol";
+import "../src/StakeAndLoan.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// A mock ERC20 token for testing purposes.
+contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
