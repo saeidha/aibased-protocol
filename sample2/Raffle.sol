97bcb33a9adce07bdd3f2ad4c86bad67793cb217
saeidha <saeed221373@yahoo.com>
2025-11-30 12:42:40 +0330

    Add state variables for Chainlink VRF in Raffle contract
    Introduced state variables for Chainlink VRF integration, including the VRF coordinator, subscription ID, gas lane, callback gas limit, and constants for request confirmations and number of words. This enhancement lays the groundwork for implementing random number generation in the raffle process.

--- sample2/Raffle.sol
+++ sample2/Raffle.sol
@@ -21,3 +21,13 @@ contract Raffle is VRFConsumerBaseV2 {
         OPEN,
         CALCULATING
     }
+
+
+    /* State Variables */
+    // Chainlink VRF variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane; // keyHash
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
