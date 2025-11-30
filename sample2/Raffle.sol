3565da4192c085fd2eae6aa66e57c1609eee96fe
saeidha <saeed221373@yahoo.com>
2025-11-30 12:41:58 +0330

    Add Raffle contract skeleton
    Introduced the initial implementation of the Raffle contract, including necessary imports for Chainlink VRF integration. This sets the foundation for future enhancements related to random number generation in the raffle process.

---
+++ sample2/Raffle.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
